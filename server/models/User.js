f0a341246f00e0e82b9fb01ed1bda9ae84c2f741
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-05-12 23:19:18 +0530

    make some more code

--- server/models/User.js
+++ server/models/User.js
@@ -10,9 +10,7 @@ const userSchema=new mongoose.Schema({
         required:true
     },
     email:{
-        type:String,
-        unique:true,
-        required:true
+        type:String
     },
     password:{
         type:String,
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express=require('express');
 const database=require('./config/db'); // import database file
+const authRoute=require('./routes/authRoutes');
 const cors=require('cors');
 const PORT=process.env.PORT || 5000;
 
@@ -8,6 +9,7 @@ const app=express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth',authRoute);
 
 //database
 database();
